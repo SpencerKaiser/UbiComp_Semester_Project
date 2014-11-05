979bdb8af27ea71aed3960c4e2c461b764a5edef
SpencerKaiser <skaiser@me.com>
2014-11-05 12:28:22 -0600

    Cleaning up old python script by removing commented out code

--- serialProject.py
+++ serialProject.py
@@ -19,21 +19,13 @@ volumes[7] = 'osascript -e "set Volume 7"'
 
 curVol = 0
 
-serial_port = serial.Serial('/dev/tty.usbmodem1451', 14400, timeout=1)
+serial_port = serial.Serial('/dev/tty.usbmodem1411', 14400, timeout=1)
 serial_port.flush()
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 pathFile = open(os.path.join(__location__, 'pathFile.txt'))
 scriptsPath = pathFile.readline()
 
-# size_of_queue = 300
-# init_queue_value = -1
-# data = deque([init_queue_value] * size_of_queue)
-
-# graph = plt.axes(xlim=(1, 300), ylim=(0, 100))
-# line, = plt.plot(data)
-# plt.ion()
-# plt.show()
 
 currLevel = 0
 val_as_int = 101
@@ -47,11 +39,6 @@ while True:
     val = serial_port.read()
     if val:
         val_as_int = ord(val)
-        # data.appendleft(val_as_int)
-        # data.pop()
-        # line.set_ydata(data)
-        # plt.draw()
-        # plt.pause(0.0001)
 
     diff = fabs(val_as_int - prevVal)
 
@@ -89,37 +76,6 @@ while True:
             waiting = True
         else:
             muted = False
-        # if 10 <= val_as_int < 14:
-        #     os.system(volumes[1])
-        #     curVol = 1
-        #     print("Volume Changed to 1")
-        # elif 14 <= val_as_int < 17:
-        #     os.system(volumes[2])
-        #     curVol = 2
-        #     print("Volume Changed to 2")
-        # elif 17 <= val_as_int < 20:
-        #     os.system(volumes[3])
-        #     curVol = 3
-        #     print("Volume Changed to 3")
-        # elif 20 <= val_as_int < 24:
-        #     os.system(volumes[4])
-        #     curVol = 4
-        #     print("Volume Changed to 4")
-        # elif 24 <= val_as_int < 28:
-        #     os.system(volumes[5])
-        #     curVol = 5
-        #     print("Volume Changed to 5")
-        # elif 28 <= val_as_int < 31:
-        #     os.system(volumes[6])
-        #     curVol = 6
-        #     print("Volume Changed to 6")
-        # elif 31 <= val_as_int < 34:
-        #     os.system(volumes[7])
-        #     curVol = 7
-        #     print("Volume Changed to 7")
-        # prevVal = val_as_int
-        # if val_as_int > 34:
-        #     waiting = True
     elif (val_as_int < 10 or val_as_int > 1000) and waiting is True:
         if muted:
             os.system(volumes[curVol])
